7b6e13cf89268ef0c337d942efd6063c653abac1
Likhith Asapu <likhith.a@research.iiit.ac.in>
2023-03-04 17:04:50 +0530

    Added all files

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# Kneyser-Ney-and-Witten-Bell-smoothing
+## Description 
+
 Implementation of Kneyser-Ney and Witten Bell smoothing techniques from scratch in Python
+
+## Instructions
+
+To run the code make sure the given packages are installed  `re,collections,random and sys`.
+
+The code can be run by entering the following command:
+
+`$ python3 language_model.py <n_value> <smoothing type> <path to corpus> `
+
+<n_value> - Value of N-gram or N-gram count(eg. 2 for using bigrams)
+
+<smoothing type> - Enter k to use Kneyser-Ney smoothing and w to use Witten bell smoothing
+
+<path to corpus> - Replace this with path to the file containing training data.
+
+You are allowed to enter only one sentence at a time.
+
+## Perplexity 
+
+The perplexity scores can be found by running the perp_score.py file.
+
+`$ python3 perp_score.py <n_value> <smoothing type> <path to corpus>  <type>`
+
+<type> - values = ("test" / "train") - Refers to whether test data should be used for perplexity calculation or train data
+
+Example - `python3 perp_score.py 4 w medical-corpus.txt test` 
+
+The average perplexity score present at the bottom of the file. The path to corpus will containing both training and testing data.
+
+## Code Testing
+
+The code has been tested on the following corpora:
+- EuroParl corpus
+- Medical Abstracts corpus
+
+<table>
+    <thead>
+        <tr>
+            <th> </th>
+            <th colspan=2>EuroParl corpus</th>
+            <th colspan=2>Medical Abstracts corpus</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td> </td>
+            <td>Test</td>
+            <td>Train</td>
+            <td>Test</td>
+            <td>Train</td>
+        </tr>
+        <tr>
+            <td>4-gram LM + Kneyser-Ney smoothing</td>
+            <td>192.59</td>
+            <td>27.63</td>
+            <td>407.53</td>
+            <td>5.04</td>
+        </tr>
+        <tr>
+            <td>4-gram LM + Witten-Bell smoothing</td>
+            <td>54.89</td>
+            <td>51.40</td>
+            <td>57.00</td>
+            <td>3.61</td>
+        </tr>
+    </tbody>
+</table>
+
+- **LM_1**: tokenization + 4-gram LM + Kneyser-Ney smoothing on EuroParl corpus
+- **LM_2**: tokenization + 4-gram LM + Witten-Bell smoothing on EuroParl corpus
+- **LM_3**: tokenization + 4-gram LM + Kneyser-Ney smoothing on Medical Abstracts corpus
+- **LM_4**: tokenization + 4-gram LM + Witten-Bell smoothing on Medical Abstracts corpus
+
+
---
+++ perp_score.py
@@ -0,0 +1,262 @@
+import re
+from collections import Counter
+import random
+import sys
+from traceback import print_list
+
+ORDER = int(sys.argv[1])
+D = 0.75
+
+def tokenisation(text,removepunc):
+  split_text = re.split("\s",text)
+  tokenised = []
+  for word in split_text:
+    if re.search(r"^#[\w\W]",word):
+      tokenised.append("<HASHTAG>")
+    elif re.search(r"^@[\w\W]",word):
+      tokenised.append("<MENTION>")
+    elif re.search(r"^http",word):
+      tokenised.append("<URL>")
+    elif re.search(r'^\d+%',word):
+      tokenised.append("<PERCENTAGE>")
+    elif re.search(r'^\d+',word):
+      tokenised.append("<NUMEXP>")
+    else:
+      if(removepunc):
+        new_word = re.sub(r"[^'\w]", "", word)
+      else:
+        new_word = re.sub(r'(\W)\1{1,}', r'\1', word)
+      new_word_list = re.findall(r"\w+|\W",new_word)
+      tokenised = tokenised + new_word_list
+
+  return tokenised
+
+
+def cleaned_output(text):
+  text = re.sub(r"^#[\w\-\_]+|\s#[\w\-\_]+"," <HASHTAG>",text)
+  text = re.sub(r"^@[\w\-\_]+|\s@[\w\-\_]+"," <MENTION>",text)
+  text = re.sub(r"http[^\s]+","<URL>",text)
+  text = re.sub(r"^\d+%|\s\d+%"," <PERCENTAGE>",text)
+  text = re.sub(r"^\d+[^\s]*|\s\d+[^\s]*"," <NUMEXP>",text)
+  text = re.sub(r'(\W)\1{1,}', r'\1', text)
+
+  return text
+
+# file = open("general-tweets.txt","r")
+# text = file.read()
+# print(cleaned_output(text))
+
+file = open(sys.argv[3],"r")
+textlist = file.readlines()
+
+random.shuffle(textlist)
+trainlist = textlist[:-1000]
+testlist = textlist[-1000:]
+
+tokenisedlist = []
+for line in trainlist:
+  addline = tokenisation(line.lower(),True)
+  addline = ['<s>'] + addline + ['</s>']
+  tokenisedlist.append(addline)
+
+def createNgrams(splitlist,k):
+  n_grams = []
+  for n in range(0,k):
+    n_grams.append(Counter())
+  for n in range(0,k):
+    for line in splitlist:
+      for i in range(0,len(line)-n):
+        n_grams[n][" ".join(line[i:i+1+n])] += 1
+  return n_grams
+
+def createContext(k,n_grams):
+  context_n_grams = []
+  next_n_grams = []
+  for n in range(0,k-1):
+    context_n_grams.append(Counter())
+    next_n_grams.append(Counter())
+
+  for n in range(0,k-1):
+    for key in n_grams[n+1].keys():
+      context_n_grams[n][" ".join(key.split()[1:])] += 1
+      next_n_grams[n][" ".join(key.split()[:-1])] += 1
+
+  return context_n_grams,next_n_grams
+
+
+def calculate_pkn(n_grams,searchgram,k,context_n_grams,next_n_grams,SUM_N):
+  count = 0
+  prevcount = 0
+  prevsearchgram = " ".join(searchgram.split()[:-1])
+  nextsearchgram = " ".join(searchgram.split()[1:])
+  if k == 1:
+    count = 0
+    if k == ORDER:
+      return ((max(n_grams[k-1][searchgram]-D,0)/sum(n_grams[k-1].values())) + ( D / sum(n_grams[k-1].values()) ))
+
+    count = context_n_grams[k-1][searchgram]
+    return ((max(count-D,0)/SUM_N) + ( D / SUM_N))
+
+  if(n_grams[k-1][searchgram] == 0):
+    return calculate_pkn(n_grams,nextsearchgram,k-1,context_n_grams,next_n_grams,SUM_N)
+
+ 
+  count = n_grams[k-1][searchgram]
+  prevcount = n_grams[k-2][prevsearchgram]
+
+    # if re.search("^<s> ",searchgram):
+    #   count = n_grams[k-1][searchgram]
+    #   prevcount = n_grams[k-2][prevsearchgram]
+
+  nextcount = next_n_grams[k-2][prevsearchgram]
+  
+  return ((max(count-D,0)/prevcount) + (( D * nextcount/ prevcount)*(calculate_pkn(n_grams,nextsearchgram,k-1,context_n_grams,next_n_grams,SUM_N))))
+    
+
+def KN_smoothing(trainset,k,testset):
+  n_grams = createNgrams(trainset,k)
+  context_n_grams,next_n_grams = createContext(k,n_grams)
+  avg_perplexity = 0
+  numberofsen = 0
+  SUM_N = 0
+  printlist = []
+  if k > 1:
+    SUM_N = sum(n_grams[1].values())
+  for testline in testset:
+    line = testline.lower()
+    line = tokenisation(line,True)
+    if len(line) == 0:
+      line = ["."]
+    newline = ["<s>"] + line + ["</s>"]
+    perp_score = 1
+    prob_count = 0
+    for i in range(0,len(newline)-k + 1):
+      searchgram = " ".join(newline[i:i+k])
+      probability = calculate_pkn(n_grams,searchgram,k,context_n_grams,next_n_grams,SUM_N)
+      perp_score = perp_score * probability
+      prob_count += 1
+    for i in range(2,k):
+      searchgram = " ".join(newline[0:i])
+      ngramorder = i
+      probability = calculate_pkn(n_grams,searchgram,ngramorder,context_n_grams,next_n_grams,SUM_N)
+      perp_score = perp_score * probability
+      prob_count += 1
+    if(perp_score == 0):
+      print("INF")
+    else:
+      perp_score = (1 / perp_score) ** (1/prob_count)
+      printlist.append([testline[:-1],"\t",perp_score])
+      avg_perplexity += perp_score
+      numberofsen += 1
+  
+  print(avg_perplexity/numberofsen)
+  for obj in printlist:
+    print(obj[0],obj[1],obj[2])
+  
+    
+def createNextgrams(k,n_grams):
+  next_n_grams = []
+  for n in range(0,k-1):
+    next_n_grams.append(Counter())
+
+  for n in range(0,k-1):
+    for key in n_grams[n+1].keys():
+      next_n_grams[n][" ".join(key.split()[:-1])] += 1
+        
+
+  return next_n_grams
+
+
+def calculate_pwn(n_grams,searchgram,k,next_n_grams,abs_bigrams):
+
+  prevsearchgram = " ".join(searchgram.split()[:-1])
+  nextsearchgram = " ".join(searchgram.split()[1:])
+  count = n_grams[k-1][searchgram]
+  prevcount = n_grams[k-2][prevsearchgram]
+
+  if k == 1:
+    return ((n_grams[0][searchgram] + 1)/(len(n_grams[0].keys()) + sum(n_grams[0].values())))
+  if k == 2:
+    if prevcount == 0:
+      return ((n_grams[0][nextsearchgram] + 1)/(len(n_grams[0].keys()) + sum(n_grams[0].values())))
+    elif count == 0:
+      return ((n_grams[0][prevsearchgram])/(abs_bigrams[prevsearchgram]*(prevcount + n_grams[0][prevsearchgram])))
+    else:
+      return (count/(prevcount + n_grams[0][prevsearchgram]))
+
+  if prevcount == 0:
+    return calculate_pwn(n_grams,nextsearchgram,k-1,next_n_grams,abs_bigrams)
+
+  nextcount = next_n_grams[k-2][prevsearchgram]
+  return ( (count + (nextcount * calculate_pwn(n_grams,nextsearchgram,k-1,next_n_grams,abs_bigrams))) / (prevcount + nextcount))
+
+def create_abs_bigrams(test_n_grams,n_grams):
+  abs_bigrams = Counter()
+  for gram in test_n_grams[1].keys():
+    if n_grams[1][gram] == 0:
+      abs_bigrams[(gram.split())[0]] += test_n_grams[1][gram]
+
+  return abs_bigrams
+  
+def WB_smoothing(trainset,k,testset):
+  n_grams = createNgrams(trainset,k)
+  next_n_grams = createNextgrams(k,n_grams)
+  printlist = []
+  tokenisedtestlist = []
+  for newline in testset:
+    addline = tokenisation(newline.lower(),True)
+    if len(addline) == 0:
+      addline = ["."]
+    addline = ['<s>'] + addline + ['</s>']
+    tokenisedtestlist.append(addline)
+
+  test_n_grams = createNgrams(tokenisedtestlist,2)
+  abs_bigrams = ""
+  if k > 1:
+    abs_bigrams = create_abs_bigrams(test_n_grams,n_grams)
+
+  avg_perplexity = 0
+  numberofsen = 0
+
+  for testline in testset:
+    line = testline.lower()
+    line = tokenisation(line,True)
+    newline = ["<s>"] + line + ["</s>"]
+    perp_score = 1
+    num_prob = 0
+    for i in range(0,len(newline)-k + 1):
+      searchgram = " ".join(newline[i:i+k])
+      probability = calculate_pwn(n_grams,searchgram,k,next_n_grams,abs_bigrams)
+      perp_score = perp_score * probability
+      num_prob += 1
+    for i in range(2,k):
+      searchgram = " ".join(newline[0:i])
+      probability = calculate_pwn(n_grams,searchgram,i,next_n_grams,abs_bigrams)
+      perp_score = perp_score * probability
+      num_prob += 1
+
+    if(perp_score == 0):
+      print("INF")
+    else:
+      perp_score = (1 / perp_score) ** (1/num_prob)
+      printlist.append([testline[:-1],"\t",perp_score])
+      avg_perplexity += perp_score
+      numberofsen += 1
+  print(avg_perplexity/numberofsen)
+  for obj in printlist:
+    print(obj[0],obj[1],obj[2])
+
+if(sys.argv[2] == 'k'):
+  if sys.argv[4] == 'test':
+    KN_smoothing(tokenisedlist,ORDER,testlist)
+  elif sys.argv[4] == 'train':
+    KN_smoothing(tokenisedlist,ORDER,trainlist)
+elif(sys.argv[2] == 'w'):
+  if sys.argv[4] == 'test':
+    WB_smoothing(tokenisedlist,ORDER,testlist)
+  elif sys.argv[4] == 'train':
+    WB_smoothing(tokenisedlist,ORDER,trainlist)
+else:
+  print("invalid")
+
+
